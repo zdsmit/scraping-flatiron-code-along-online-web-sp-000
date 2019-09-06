8758e9e7a41a46de83d3310e4a0a546273fcfb79
Zach  Smith <zdsmith001@gmail.com>
2019-09-06 18:24:37 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -37,7 +37,7 @@ class Scraper
     self.get_courses.each do |post|
       course = Course.new
       course.title = post.css("h2").text
-      course.schedule = post.css("date").text
+      course.schedule = post.css(".date").text
       course.description = post.css("p").text
     end
   end
